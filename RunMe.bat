d0f19ad4b75082d2d2e0e454b740cba0dd63ac1f
Prashant <prashant.mundepi@omegapoint.se>
2018-10-05 14:26:02 +0200

    new change

---
+++ RunMe.bat
@@ -0,0 +1,5 @@
+set projectLocation = /Users/prashantmundepi/Documents/Selenium_projects/DataDrivenFramework
+cd %projectLocation%
+set classpath =%projectLocation%\bin;%projectLocation%\Jars\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
